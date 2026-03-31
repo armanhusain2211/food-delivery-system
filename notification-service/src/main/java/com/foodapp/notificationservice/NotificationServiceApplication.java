ae2052243ca25fbcf6b1b96c7a76ef3a002ef4eb
Arman Husain <armanhusain2211@gmail.com>
2026-04-01 00:01:50 +0530

    fix

--- notification-service/src/main/java/com/foodapp/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/foodapp/notificationservice/NotificationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.foodapp;
+package com.foodapp.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
